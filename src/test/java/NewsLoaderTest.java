3ccc2b9e08a5fcf17abbeb74d8be68bb2e72cb7a
Wojciech Pełka <nero1993@onet.pl>
2016-04-19 13:44:50 +0200

    Dodanie metody testującej przypadek publicznej zawartosci PublicContentTest przy wykonywaniu metody PrepareForPublish

--- src/test/java/NewsLoaderTest.java
+++ src/test/java/NewsLoaderTest.java
@@ -1,33 +1,56 @@
-import edu.iis.mto.staticmock.Configuration;
-import edu.iis.mto.staticmock.ConfigurationLoader;
-import edu.iis.mto.staticmock.IncomingNews;
-import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.*;
 import edu.iis.mto.staticmock.reader.NewsReader;
 import org.junit.Before;
-import org.mockito.*;
-import org.powermock.api.mockito.*;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by wojciech.pelka on 2016-04-19.
  */
+
 public class NewsLoaderTest
 {
     ConfigurationLoader configurationLoader;
-    NewsReaderFactory newsReaderFactory;
+    NewsReader newsReader;
+    NewsLoader newsLoader;
+    IncomingNews incomingNews;
+
 
     @Before
     public void init()
     {
+        newsLoader = new NewsLoader();
+        incomingNews = new IncomingNews();
+        incomingNews.add(new IncomingInfo("SubA",SubsciptionType.A));
+        incomingNews.add(new IncomingInfo("NoneSub1",SubsciptionType.NONE));
+        incomingNews.add(new IncomingInfo("NoneSub2",SubsciptionType.NONE));
+
+
         configurationLoader = Mockito.mock(ConfigurationLoader.class);
         Mockito.when(configurationLoader.loadConfiguration()).thenReturn(new Configuration());
         PowerMockito.mockStatic(ConfigurationLoader.class);
         PowerMockito.when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
 
-
-        NewsReader newsReader = Mockito.mock(NewsReader.class);
+        newsReader = Mockito.mock(NewsReader.class);
         Mockito.when(newsReader.read()).thenReturn(new IncomingNews());
 
         PowerMockito.mockStatic(NewsReaderFactory.class);
         PowerMockito.when(NewsReaderFactory.getReader(Mockito.anyString())).thenReturn(newsReader);
+    }
+
+    @Test
+    public void PublicContentTest()
+    {
+        Mockito.when(newsReader.read()).thenReturn(incomingNews);
+
+        PublishableNews pn = newsLoader.loadNews();
+
+        assertThat(pn.getPublicContent().size(),is(2));
     }
 
 
